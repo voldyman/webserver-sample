0f013a1e7992bbc7e5e242ac7f0daa5cf47b5f70
Akshay Shekher <voldyman666@gmail.com>
2015-02-09 14:46:51 +0530

    Better index page

--- src/server.cc
+++ src/server.cc
@@ -79,43 +79,11 @@ web_server::index_handler(const incoming_things& incoming,
                           outgoing_things& outgoing)
 {
   ostringstream sout;
-
-  sout << " <html> <body> "
-       << "<form action='/form_handler' method='post'> "
-       << "User Name: <input name='user' type='text'><br>  "
-       << "User password: <input name='pass' type='text'> <input type='submit'> "
-       << " </form>";
-
-  sout << "<br>  path = "         << incoming.path << endl;
-  sout << "<br>  request_type = " << incoming.request_type << endl;
-  sout << "<br>  content_type = " << incoming.content_type << endl;
-  sout << "<br>  protocol = "     << incoming.protocol << endl;
-  sout << "<br>  foreign_ip = "   << incoming.foreign_ip << endl;
-  sout << "<br>  foreign_port = " << incoming.foreign_port << endl;
-  sout << "<br>  local_ip = "     << incoming.local_ip << endl;
-  sout << "<br>  local_port = "   << incoming.local_port << endl;
-  sout << "<br>  body = \""       << incoming.body << "\"" << endl;
-
-
-
-  // Echo any cookies back to the client browser
-  sout << "<h2>Cookies the web browser sent to the server</h2>";
-  for ( key_value_map::const_iterator ci = incoming.cookies.begin(); ci != incoming.cookies.end(); ++ci )
-  {
-    sout << "<br/>" << ci->first << " = " << ci->second << endl;
-  }
-
-  sout << "<br/><br/>";
-
-  sout << "<h2>HTTP Headers the web browser sent to the server</h2>";
-  // Echo out all the HTTP headers we received from the client web browser
-  for ( key_value_map_ci::const_iterator ci = incoming.headers.begin(); ci != incoming.headers.end(); ++ci )
-  {
-    sout << "<br/>" << ci->first << ": " << ci->second << endl;
-  }
-
-  sout << "</body> </html>";
-
+  sout << "<ol>"
+       << "<li> <a href='/form_handler'>Form Handling </a></li>"
+       << "<li> <a href='/ipc'> Inter Thread communication </a></li>"
+       << endl;
+    
   return sout.str();
 
 }
